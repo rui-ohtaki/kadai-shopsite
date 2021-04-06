44a60b3d8cb410a303a61186cdeaa891e87962ca
Ubuntu <ubuntu@ip-172-31-83-86.ec2.internal>
2021-04-06 07:46:59 +0000

    first commit

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel-pc').slick({
+        autoplay: true, // 自動再生
+        autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+        arrows: false, // 左右のナビゲーションを無効にする
+        dots: true, // スライド下にドットを表示する
+        fade: true, // スライドの切り替えをフェードにする
+    });
+        $('.carousel-sp').slick({
+        autoplay: true, // 自動再生
+        autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+        arrows: false, // 左右のナビゲーションを無効にする
+        dots: true, // スライド下にドットを表示する
+        fade: true, // スライドの切り替えをフェードにする
+    });
+    
+});
---
+++ js/slideshow.js
@@ -0,0 +1,6 @@
+$(window).on("load", function() {
+    $("#thumbnail img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#slideshow-image img").attr("src", img_src);
+    });
+});
