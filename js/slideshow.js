97a57860dbd5cd5cb5dc51f23de2cfa700087f71
Ubuntu <ubuntu@ip-172-31-83-86.ec2.internal>
2021-04-11 10:03:09 +0000

    apply again

--- js/slideshow.js
+++ js/slideshow.js
@@ -3,4 +3,16 @@ $(window).on("load", function() {
         let img_src = $(this).attr("src");
         $("#menu-body-1 img").attr("src", img_src);
     });
+        $("#menu-body-bread-2 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#menu-body-bread-1 img").attr("src", img_src);
+    });
+        $("#menu-body-pasta-2 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#menu-body-pasta-1 img").attr("src", img_src);
+    });
+        $("#menu-body-sweets-2 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#menu-body-sweets-1 img").attr("src", img_src);
+    });
 });
